acf3c857d0eb1a8c72cac71510a3326fafbe3612
Dong <0x317374@gmail.com>
2021-07-17 23:04:43 +0700

    chore: wip

--- inspect/inject.js
+++ inspect/inject.js
@@ -9,7 +9,9 @@ function inject() {
 
   const $group = $('[data-xviz="machine-group"]');
   const $body = $("body");
+  // https://stackoverflow.com/a/60235061/6435579
   const control = {
+    m: [1, 0, 0, 1, 0, 0],
     scaleAmount: 1.1,
     minScale: 0.5,
     maxScale: 2.0,
@@ -29,10 +31,42 @@ function inject() {
       },
     },
     dirty: true,
+    update() {
+      this.dirty = false;
+      this.m[3] = this.m[0] = this.scale;
+      this.m[2] = this.m[1] = 0;
+      this.m[4] = this.translate.x;
+      this.m[5] = this.translate.y;
+    },
     applyTo() {
-      const m = [this.scale, 0, 0, this.scale, this.translate.x, this.translate.y];
+      if (this.dirty) {
+        this.update();
+      }
 
-      $group.css("transform", `matrix(${m[0]},${m[1]},${m[2]},${m[3]},${m[4]},${m[5]})`);
+      $group.css(
+        "transform",
+        `matrix(${this.m[0]},${this.m[1]},${this.m[2]},${this.m[3]},${this.m[4]},${this.m[5]})`,
+      );
+    },
+    pan(amount) {
+      if (this.dirty) {
+        this.update();
+      }
+
+      this.translate.x += amount.x;
+      this.translate.y += amount.y;
+      this.dirty = true;
+    },
+    scaleAt(at, amount) {
+      // at in screen coords
+      if (this.dirty) {
+        this.update();
+      }
+
+      this.scale *= amount;
+      this.translate.x = at.x - (at.x - this.translate.x) * amount;
+      this.translate.y = at.y - (at.y - this.translate.y) * amount;
+      this.dirty = true;
     },
   };
   const _autoLayout = () => {
@@ -59,26 +93,31 @@ function inject() {
     e.stopPropagation();
 
     const rect = $(this)[0].getBoundingClientRect();
-    const lastZoomValue = control.scale;
-    let amount;
-
-    if (e.originalEvent["wheelDelta"] / 120 > 0) {
-      amount = control.scaleAmount;
-    } else {
-      amount = 1 / control.scaleAmount;
-    }
-
-    control.scale *= amount;
-    // control.zoomValue = Math.min(Math.max(control.minZoom, control.zoomValue), control.maxZoom);
-    // amount = control.zoomValue / lastZoomValue;
-
     const x = e.pageX - rect.width / 2;
     const y = e.pageY - rect.height / 2;
 
-    control.translate.x = x - (x - control.translate.x) * amount;
-    control.translate.y = y - (y - control.translate.y) * amount;
+    if (e.originalEvent["wheelDelta"] / 120 > 0) {
+      control.scaleAt({ x, y }, control.scaleAmount);
+      control.applyTo();
+    } else {
+      control.scaleAt({ x, y }, 1 / control.scaleAmount);
+      control.applyTo();
+    }
 
-    control.applyTo();
+    // const lastZoomValue = control.scale;
+    // let amount;
+    //
+    // if (e.originalEvent["wheelDelta"] / 120 > 0) {
+    //   amount = control.scaleAmount;
+    // } else {
+    //   amount = 1 / control.scaleAmount;
+    // }
+    //
+    // control.scale *= amount;
+    // // control.zoomValue = Math.min(Math.max(control.minZoom, control.zoomValue), control.maxZoom);
+    // // amount = control.zoomValue / lastZoomValue;
+    //
+    // control.applyTo();
   });
   $body.on("wheel", function (e) {
     e.stopPropagation();
@@ -108,8 +147,10 @@ function inject() {
       const b = control.translate.start.y - e.pageY;
       const c = Math.sqrt(a * a + b * b);
 
-      control.translate.x += e.pageX - control.translate.temp.x;
-      control.translate.y += e.pageY - control.translate.temp.y;
+      control.pan({
+        x: e.pageX - control.translate.temp.x,
+        y: e.pageY - control.translate.temp.y,
+      });
 
       if (c > control.translate.distance) {
         control.applyTo();
